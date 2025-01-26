2503dd6221bb921f47a826d91631715806510d97
Sevein Nieves <0914473@my.scccd.edu>
2025-01-26 10:24:56 -0800

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,24 @@
-console.log("Week 1 - Hello World");
+console.log('hello from inside the main.js file');
+
+//let myVar;
+const myVar = "sevein";
+const myVarType = typeof(myVar);
+
+console.log("myVarType" + myVarType);
+console.log(`myVarType ${myVarType}`);
+//below is a code block
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will 11 this one run`);
+    } else {
+        console.log(`will 13 this one run`);
+    }
+}
+
+runNow();
+runNow();
+
+
+

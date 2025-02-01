5de193e326a9b73c021684f8a8f39270fbc8b895
Sevein Nieves <0914473@my.scccd.edu>
2025-01-31 23:42:34 -0800

    need some help

--- w3/main.js
+++ w3/main.js
@@ -1,4 +1,5 @@
 let carbonfootprintPoints = 0;
+const numberInHousehold =(5)
 function determineHouseHPoints(numberInHousehold) {
     console.log("Inside the function")
     if (numberInHousehold === 1) {
@@ -21,6 +22,23 @@ console.log(`Based on the number of members of the houshold of ${numberInHouseho
 
 determineHouseHPoints(3)
 determineHouseHPoints(4)
+
+function calculateHomeFprint (size, efficiency) {
+homeCarbonPoints = 0;
+if (efficiency === "low") {
+    homeCarbonPoints + (size * 10);
+} else if (efficiency === "med") {
+homeCarbonPoints + (size * 7);
+} else if (efficiency === "high") {
+    homeCarbonPoints + (size * 4);
+} else {
+    console.log(`For a ${size} sq. ft home with ${efficiency} efficiency, the carbon footprint is ${homeCarbonPoints} kg CO2!`)
+} }
+
+calculateHomeFprint(1500, "medium")
+calculateHomeFprint(2000, "low")
+
+    console.log("lets see your score")
 
 // don't forget to use ` instead of '
 //I messed up by having the terminal stuck on my pri rather than my pub.

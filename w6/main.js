59605a7eb6549e3081065a56eaa2d7bc5d7aa33f
Sevein Nieves <0914473@my.scccd.edu>
2025-02-25 14:51:39 -0800

    my attempt at code, before I review other students code

--- w6/main.js
+++ w6/main.js
@@ -37,9 +37,10 @@ function determineHouseHoldPts(numberInHousehold) {
   return houseHoldPoints;
 }
 
-function start(houseHoldMembers, houseSize) {
+function start(houseHoldMembers, houseSize, firstName, lastName) {
   const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
   const houseSizePts = determineHouseSizePts(houseSize);
+  const fullName = `${firstName} ${lastName}`;
   const total = houseHoldPTS + houseSizePts;
 
   const cfpObj = {
@@ -47,6 +48,7 @@ function start(houseHoldMembers, houseSize) {
     houseSize: houseSize,
     houseHoldPoints: houseHoldPTS,
     houseSizePoints: houseSizePts,
+    fullName,
     cfpTotal: total,
   };
   cfpData.push(cfpObj);
@@ -56,13 +58,16 @@ function displayOutObj() {
 const output = document.getElementById("output");
     for (obj of cfpData) {
   const newH2 = document.createElement("h2");
-  newH2.textContent = `Carbon Footprint ${obj.cfpTotal}`;
   const newH3 = document.createElement("h3");
+  newH2.textContent = `Hello there ${obj.fullName}`
+  newH3.textContent = `Carbon Footprint ${obj.cfpTotal}`;
+  const newH4 = document.createElement("h4");
   const newP = document.createElement("p");
   newP.textContent = `This number is based on the number of members of the household of ${obj.houseHoldMembers} (score: ${obj.houseHoldPoints}) `;
   newP.textContent += `and a ${obj.houseSize} size of home (score: ${obj.houseSizePoints}).`;
   OUTPUT.appendChild(newH2);
   OUTPUT.appendChild(newH3);
+  OUTPUT.appendChild(newH4)
   OUTPUT.appendChild(newP);
   }
 }
@@ -105,7 +110,7 @@ FORM.addEventListener('submit', function(e){
   const lastName = FORM.lastName.value;
   const houseMembers = parseInt(FORM.housem.value);
   const houseSize = FORM.houses.value;
-  start(houseMembers, houseSize);
+  start(houseMembers, houseSize, firstName, lastName);
   OUTPUT.innerHTML = ""
   displayOutObj();
   FORM.reset();

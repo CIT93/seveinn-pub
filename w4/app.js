d746ac25d90cb66707e7c95e3de8f3764fe1efc4
Sevein Nieves <0914473@my.scccd.edu>
2026-02-07 18:19:38 -0800

    Step 1 Complete

---
+++ w4/app.js
@@ -0,0 +1,18 @@
+import * as orderForm from "./order-handler.js";
+
+const customOrderForm = document.getElementById('order-form');
+
+const orderSummary = document.querySelector('order-summary');
+
+
+const handleFormSubmit = function(event) {
+    event.preventDefault();
+    console.log(event);
+    orderForm.getOrderInputs();
+};
+
+const init = function() {
+    customOrderForm.addEventListener('submit', handleFormSubmit);
+};
+
+document.addEventListener('DOMContentLoaded', init);

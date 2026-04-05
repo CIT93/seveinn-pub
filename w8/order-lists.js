0833c5d91ef75247510c38f6bbf12a798dff2969
Sevein Nieves <0914473@my.scccd.edu>
2026-04-05 13:39:42 -0700

    table event listener wired

--- w8/order-lists.js
+++ w8/order-lists.js
@@ -36,6 +36,22 @@ const createTableRow = function(entry){
     return row;
 };
 
+const tableBody = document.getElementById('order-table-body');
+
+tableBody.addEventListener('click', function(event) {
+    const target = event.target;
+    
+    // 1. Get the ID from the button that was clicked
+    const id = target.dataset.id;
+
+    // 2. Guard Clause: If they clicked a row (white space) but NOT a button, 
+    // there will be no ID. So we stop the function immediately.
+    if (!id) return;
+
+    // 3. Temporary Test: Log the ID to prove it works!
+    console.log("Clicked button with ID:", id); 
+});
+
 export const renderTable = function(orders) {
     orderTableBody.innerHTML = '';
 
